c78b181a3d3325501374687973718c5f27e88b41
Masaiki <mydarer@gmail.com>
2019-05-15 23:07:18 +0800

    增加高DPI支持 修复偶现白块bug

---
+++ GetColor/ColorPicker.cs
@@ -0,0 +1,156 @@
+﻿// Xianfen.Net.GetColor.ColorPicker
+using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GetColor
+{
+
+    public partial class ColorPicker : Form
+    {
+        private PictureBox pictureBox;
+
+        private Bitmap bmp;
+
+        //private Graphics g;
+
+        private bool isDown;
+
+        private Point downPoint;
+
+        private Panel pnlColorWindow;
+
+        private TrackBar tckZoomSize;
+
+        private PictureBox picZoom;
+
+        private Label lblYValue;
+
+        private Label lblYName;
+
+        private Label lblXValue;
+
+        private Label lblXName;
+
+        private Label lblShowColor;
+
+        private TextBox txtColorValue;
+
+        //private Rectangle rect;
+
+        private Pen pen;
+
+        private Bitmap bmpZoom;
+
+        private Graphics zoom;
+
+        private int zoomSize;
+
+        private Color selectedColor = Color.White;
+
+        public Color SelectedColor
+        {
+            get
+            {
+                return selectedColor;
+            }
+            set
+            {
+                selectedColor = value;
+            }
+        }
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+            pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.BorderStyle = BorderStyle.None;
+            pictureBox.MouseUp += pictureBox_MouseUp;
+            pictureBox.MouseMove += pictureBox_MouseMove;
+            pictureBox.Cursor = Cursors.Cross;
+            base.Controls.Add(pictureBox);
+            base.Size = new Size(0, 0);
+            DoubleBuffered = true;
+            base.Load += ColorPicker_Load;
+            bmpZoom = new Bitmap(picZoom.Width, picZoom.Height);
+            zoom = Graphics.FromImage(bmpZoom);
+            picZoom.Image = bmpZoom;
+            pen = new Pen(Color.Black, 1f);
+            pen.DashCap = DashCap.Round;
+            pen.DashStyle = DashStyle.Dash;
+            zoomSize = tckZoomSize.Value;
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePosition = Control.MousePosition;
+            lblXValue.Text = mousePosition.X.ToString();
+            lblYValue.Text = mousePosition.Y.ToString();
+            Color pixel = bmp.GetPixel(mousePosition.X, mousePosition.Y);
+            lblShowColor.BackColor = pixel;
+            txtColorValue.Text = string.Format("#{0,2:X2}{1,2:X2}{2,2:X2}", pixel.R.ToString("X"), pixel.G.ToString("X"), pixel.B.ToString("X")).Replace(" ", "0");
+            zoom.InterpolationMode = InterpolationMode.NearestNeighbor;
+            zoom.DrawImage(bmp, new Rectangle(0, 0, bmpZoom.Width, bmpZoom.Height), Control.MousePosition.X - bmpZoom.Width / (2 * zoomSize), Control.MousePosition.Y - bmpZoom.Height / (2 * zoomSize), bmpZoom.Width / zoomSize, bmpZoom.Height / zoomSize, GraphicsUnit.Pixel);
+            zoom.DrawLine(pen, picZoom.Width / 2 - 1, 0, picZoom.Width / 2 - 1, picZoom.Height);
+            zoom.DrawLine(pen, 0, picZoom.Height / 2 - 1, picZoom.Width, picZoom.Height / 2 - 1);
+            picZoom.Refresh();
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            selectedColor = bmp.GetPixel(e.Location.X, e.Location.Y);
+            pen.Dispose();
+            bmp.Dispose();
+            bmpZoom.Dispose();
+            zoom.Dispose();
+            //g.Dispose();
+            Close();
+        }
+
+        private void ColorPicker_Load(object sender, EventArgs e)
+        {
+            bmp = ScreenPicture.GetFullScreen();
+            pictureBox.Image = bmp;
+            base.TopMost = true;
+            base.FormBorderStyle = FormBorderStyle.None;
+            base.WindowState = FormWindowState.Maximized;
+        }
+
+        private void pnlColorWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDown = true;
+                downPoint = e.Location;
+            }
+        }
+
+        private void pnlColorWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                pnlColorWindow.Left = Control.MousePosition.X - downPoint.X;
+                pnlColorWindow.Top = Control.MousePosition.Y - downPoint.Y;
+            }
+        }
+
+        private void pnlColorWindow_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDown = false;
+            }
+        }
+
+        private void tckZoomSize_ValueChanged(object sender, EventArgs e)
+        {
+            zoomSize = tckZoomSize.Value;
+        }
+
+    }
+
+}
---
+++ GetColor/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GetColor
+{
+
+    public partial class MainForm : Form
+    {
+        private Label lblColor0;
+
+        private Button btnPickColor;
+
+        private Button btnSelectColor;
+
+        private Button btnAbout;
+
+        private GroupBox gpbColorStack;
+
+        private Label lblColor5;
+
+        private Label lblColor4;
+
+        private Label lblColor3;
+
+        private Label lblColor2;
+
+        private Label lblColor1;
+
+        private GroupBox gpbValue;
+
+        private Label lblColor7;
+
+        private Label lblColor6;
+
+        private Label lblCMYK;
+
+        private Label lblARGB;
+
+        private Label lblRGB;
+
+        private TextBox txtHSB;
+
+        private TextBox txtARGB;
+
+        private TextBox txtRGB;
+
+        private ContextMenuStrip ctnMenu;
+
+        private ToolStripMenuItem toolPickColor;
+
+        private ToolStripMenuItem toolShow;
+
+        private ToolStripMenuItem toolAbout;
+
+        private ToolStripSeparator toolStripMenuItem1;
+
+        private ToolStripMenuItem toolClose;
+
+        private ColorDialog colorDlg;
+
+        private Label label1;
+
+        private TextBox txtBGR;
+
+        private Label lab;
+
+        private Timer timer1;
+
+        private Label label2;
+
+        private TextBox txtRGBdec;
+
+        private Label label3;
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorPicker colorPicker = new ColorPicker();
+            Hide();
+            colorPicker.ShowDialog();
+            Show();
+            BackColor = colorPicker.SelectedColor;
+        }
+
+        private void btnPickColor_Click(object sender, EventArgs e)
+        {
+            using (ColorPicker colorPicker = new ColorPicker())
+            {
+                base.WindowState = FormWindowState.Minimized;
+                base.Visible = false;
+                colorPicker.ShowDialog();
+                for (int num = 7; num > 0; num--)
+                {
+                    ((Label)base.Controls.Find("lblColor" + num.ToString(), searchAllChildren: true)[0]).BackColor = ((Label)base.Controls.Find("lblColor" + (num - 1).ToString(), searchAllChildren: true)[0]).BackColor;
+                }
+                lblColor0.BackColor = colorPicker.SelectedColor;
+                toolShow_Click(null, null);
+                lblColor_Click(lblColor0, null);
+            }
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            colorDlg.Color = lblColor0.BackColor;
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                lblColor0.BackColor = colorDlg.Color;
+                lblColor_Click(lblColor0, null);
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("小丸为获取用于ASS字幕的BGR颜色值而修改此软件。\n原作者：周振兴 Blog：http://www.xianfen.net \n发布日期：2013年12月11日\n2019年5月15日更新 菜姬修改以适配高DPI 修复部分区域白块问题", "关于 小丸拾色器", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lnkCopyRights_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.xianfen.net");
+        }
+
+        private void lblColor_Click(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            if (label != null)
+            {
+                Color backColor = label.BackColor;
+                txtRGB.Text = string.Format("{0,2:X2}{1,2:X2}{2,2:X2}", backColor.R.ToString("X"), backColor.G.ToString("X"), backColor.B.ToString("X")).Replace(" ", "0");
+                txtRGBdec.Text = $"{backColor.R.ToString()} {backColor.G.ToString()} {backColor.B.ToString()}";
+                txtARGB.Text = string.Format("{0,3:D3} {1,3:D3} {2,3:D3} {3,2:D3}", backColor.A, backColor.R, backColor.G, backColor.B);
+                txtBGR.Text = string.Format("&H00{0,2:X2}{1,2:X2}{2,2:X2}", backColor.B.ToString("X"), backColor.G.ToString("X"), backColor.R.ToString("X")).Replace(" ", "0");
+                int num = (int)((double)(backColor.GetHue() / 360f * 240f) + 0.5);
+                int num2 = (int)((double)(backColor.GetSaturation() * 241f) + 0.5);
+                int num3 = (int)((double)(backColor.GetBrightness() * 241f) + 0.5);
+                if (num > 239)
+                {
+                    num = 239;
+                }
+                if (num2 > 240)
+                {
+                    num2 = 240;
+                }
+                if (num3 > 240)
+                {
+                    num3 = 240;
+                }
+                txtHSB.Text = string.Format("{0,3:D3} {1,3:D3} {2,3:D3}", num, num2, num3);
+            }
+        }
+
+        private void lblColor_DoubleClick(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            if (label != null)
+            {
+                Color backColor = label.BackColor;
+                colorDlg.Color = backColor;
+                if (colorDlg.ShowDialog() == DialogResult.OK)
+                {
+                    label.BackColor = colorDlg.Color;
+                }
+            }
+        }
+
+        private void toolShow_Click(object sender, EventArgs e)
+        {
+            Show();
+            base.WindowState = FormWindowState.Normal;
+            Activate();
+        }
+
+        private void ntyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            toolShow_Click(null, null);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lab.Text = "";
+            timer1.Enabled = false;
+        }
+
+        private void txtBGR_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text != "")
+            {
+                Clipboard.SetDataObject(textBox.Text);
+                lab.Text = "已经复制到剪贴板！";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void txtHSB_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text != "")
+            {
+                Clipboard.SetDataObject(textBox.Text);
+                lab.Text = "已经复制到剪贴板！";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void txtRGBdec_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text != "")
+            {
+                Clipboard.SetDataObject(textBox.Text);
+                lab.Text = "已经复制到剪贴板！";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void txtRGB_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text != "")
+            {
+                Clipboard.SetDataObject(textBox.Text);
+                lab.Text = "已经复制到剪贴板！";
+                timer1.Enabled = true;
+            }
+        }
+    }
+
+}
